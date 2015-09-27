640db67675ce4f500863479121d4d0ae4e08dbd7
Jared Moore <jaredmoo@microsoft.com>
2015-09-27 12:48:01 -0700

    p002.fsx

---
+++ fs/p002.fsx
@@ -0,0 +1,22 @@
+﻿let max = 4000000
+
+// Functional solution
+let fibs = 
+    Seq.unfold 
+        (fun (a, b) -> Some (a, (b, a + b)))
+        (1, 2)
+
+fibs |> Seq.takeWhile (fun n -> n < max)
+     |> Seq.filter (fun n -> n % 2 = 0)
+     |> Seq.sum
+
+// Procedural solution
+//open System.Linq
+//
+//let fibs = System.Collections.Generic.List<int>()
+//fibs.Add 1
+//fibs.Add 2
+//while fibs.Last() < max do
+//    fibs.Add (fibs.Item (fibs.Count-1) + fibs.Item (fibs.Count-2))
+//
+//fibs |> Seq.where (fun n -> n % 2 = 0) |> Seq.sum
